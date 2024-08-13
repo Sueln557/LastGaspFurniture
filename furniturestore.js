480a195706794b2f953ee89f79fa696f69235ace
Sueln557 <sueling.ng@bellevuecollege.edu>
2024-08-13 08:23:37 -0700

    Update furniturestore.js
    Update furniturestore.js

--- furniturestore.js
+++ furniturestore.js
@@ -2,7 +2,7 @@ let sum_item_total = 0;
 
 function furnstore() {
     sum_item_total = 2 + 3;
-    console.log(sum_item_total);
+    //console.log(sum_item_total);
 
     // Redirect to invoice.html with the sum_item_total as a query parameter
     window.location.href = `invoice.html?total=${sum_item_total}`;
